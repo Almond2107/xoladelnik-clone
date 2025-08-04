5f13b21293eef1cd4c9142c1ef5cd68731c62c46
Almond2107 <almond2100@gmail.com>
2025-08-04 09:51:32 -0400

    initiall_commit

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+# Create your views here.
+
+@login_required
+def profile(request):
+    """Display current user profile page."""
+    return render(request, "profile.html")
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def product_single(request):
+    return render(request, 'product-single.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
