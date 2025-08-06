97119496a6d1fce2e45a0eacd4d48ec5894d6cbd
Almond2107 <almond2100@gmail.com>
2025-08-06 08:44:20 -0400

    updated static file

--- core/urls.py
+++ core/urls.py
@@ -25,10 +25,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include("common.urls")),
+    path("users/", include("users.urls")),
 ]
 
-
-# Static files (development) – serve from STATICFILES_DIRS directly
-urlpatterns += staticfiles_urlpatterns()
-# Media files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
